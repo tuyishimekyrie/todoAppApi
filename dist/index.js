59c25f83422deeda6e817384b59e1d27f46b5895
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-02-28 10:18:02 +0200

    uncessary changes in dist

--- dist/index.js
+++ dist/index.js
@@ -3,18 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+// require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const todos_1 = __importDefault(require("./src/routers/todos"));
 const users_1 = __importDefault(require("./src/routers/users"));
 const auth_1 = __importDefault(require("./src/routers/auth"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const config_1 = __importDefault(require("config"));
 const app = (0, express_1.default)();
-if (!config_1.default.get("jwtPrivateKey")) {
-    console.error("FATAL ERROR: jwtPrivateKey is not defined");
-    process.exit(1);
-}
+// if (!config.get("jwtPrivateKey")) {
+//   console.error("FATAL ERROR: jwtPrivateKey is not defined");
+//   process.exit(1);
+// }
 mongoose_1.default
     .connect("mongodb://localhost:27017/todoAppApi")
     .then(() => console.log("Database Running"))
